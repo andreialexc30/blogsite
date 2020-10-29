c3edbe3a4943f5a1baf33e55803e6b100da13bed
Andrei Alexandru Caia <andreialexx30@gmail.com>
2020-10-29 11:48:30 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var page = 1;
+
+
+$(document).ready(function(){
+    
+    $('#next').click(function() {
+        $('#' + page).hide();
+        page = page +1;
+        $('#' + page).show();
+    })
+});
+
+$('#previous').click(function() {
+    $('#' + page).hide();
+    page = page - 1;
+    $('#' + page).show();
+});
