d557a2ea832148d2b9a37f9ac65a5407228cd762
Andrei Alexandru Caia <andreialexx30@gmail.com>
2020-10-31 12:56:52 +0200

    Added onclick function for some buttons

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 var page = 1;
 
+document.getElementById('pressBtn').onclick = () => {
+    location.href = "https://static1.squarespace.com/static/50363cf324ac8e905e7df861/t/5ed23bd3bdbbdb299cb6aa9f/1590836452410/2020+Benedict+Evans+Shoulders+of+Giants.pdf";
+};
+
+document.getElementById('subBtn').onclick = () => {
+    location.href = "newsletter.html";
+};
 
 $(document).ready(function(){
     
